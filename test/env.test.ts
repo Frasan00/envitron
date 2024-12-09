e299d3f3e7acf44780c9ed7a57698788ac0defa4
Frasan00 <francesco.sangiovanni.7@gmail.com>
2024-12-09 18:22:54 +0100

    feature: test case

---
+++ test/env.test.ts
@@ -0,0 +1,93 @@
+import fs from 'node:fs';
+import { createEnvSchema, getInstance } from '../src/index';
+
+beforeAll(async () => {
+  const envContent = fs.readFileSync('.env.example', 'utf-8');
+  if (fs.existsSync('.env')) {
+    fs.unlinkSync('.env');
+  }
+
+  fs.writeFileSync('.env', envContent);
+});
+
+afterAll(() => {
+  if (fs.existsSync('.env')) {
+    fs.unlinkSync('.env');
+  }
+});
+
+test('env manager', async () => {
+  const env = await createEnvSchema((z) =>
+    z.object({
+      DEFAULT_ENUM: z.enum(["test", "test2"]).default("test"),
+      DEFAULT_STRING: z.string().default("test"),
+      DEFAULT_NUM: z.number().default(1),
+      DEFAULT_BOOLEAN: z.boolean().default(false),
+      NODE_ENV: z.string().optional(),
+      COMMENTED_ENV: z.string().optional(),
+      SEMI_COMMENTED_ENV: z.string().optional(),
+      DATABASE_URL: z.string().optional(),
+      API_KEY: z.string().optional(),
+      DEBUG: z.boolean().optional(),
+      EMPTY_VALUE: z.string().optional(),
+      QUOTED_EMPTY_VALUE: z.string().optional(),
+      SINGLE_QUOTED_EMPTY_VALUE: z.string().optional(),
+      SPACED_KEY: z.string().optional(),
+      SPACED_KEY_WITH_QUOTES: z.string().optional(),
+      SPECIAL_CHARS_IN_VALUE: z.string().optional(),
+      TRAILING_SPACES: z.string().optional(),
+      LIST_OF_VALUES_WITH_QUOTES: z.array(z.union([z.string(), z.number()])).optional(),
+      LIST_OF_VALUES_WITH_SINGLE_QUOTES: z.array(z.string()).optional(),
+      LIST_OF_VALUES_WITHOUT_QUOTES: z.array(z.string()).optional(),
+      OBJECT: z.object({
+        key: z.string()
+      }).optional(),
+    })
+  );
+
+  expect(env.get('NODE_ENV')).toBe("development");
+  expect(env.get('DATABASE_URL')).toBe(" TESTTT ");
+  expect(env.get('API_KEY')).toBe(" 12345 ");
+  expect(env.get('DEBUG')).toBe(true);
+  expect(env.get('EMPTY_VALUE')).toBe(undefined);
+  expect(env.get('QUOTED_EMPTY_VALUE')).toBe('');
+  expect(env.get('SINGLE_QUOTED_EMPTY_VALUE')).toBe('');
+  expect(env.get('SPACED_KEY')).toBe("spaced_value");
+  expect(env.get('SPACED_KEY_WITH_QUOTES')).toBe(" spaced_value ");
+  expect(env.get('SPECIAL_CHARS_IN_VALUE')).toBe("!@#$%^&*()_+");
+  expect(env.get('TRAILING_SPACES')).toBe("trailing_spaces");
+  expect(env.get('LIST_OF_VALUES_WITH_QUOTES')).toEqual(['0', '1']);
+  expect(env.get('LIST_OF_VALUES_WITH_SINGLE_QUOTES')).toEqual([' example', "example"]);
+  expect(env.get('LIST_OF_VALUES_WITHOUT_QUOTES')).toEqual(["example", "example"]);
+  expect(env.get('OBJECT')).toEqual({ key: "value" });
+  expect(env.get('COMMENTED_ENV')).toBe(undefined);
+  expect(env.get('SEMI_COMMENTED_ENV')).toBe("sh");
+
+  const allEnvs = env.getAll();
+  console.log(allEnvs);
+});
+
+test('Single Instance', async () => {
+  const env = getInstance();
+
+  expect(env.get('NODE_ENV')).toBe("development");
+  expect(env.get('DATABASE_URL')).toBe(" TESTTT ");
+  expect(env.get('API_KEY')).toBe(" 12345 ");
+  expect(env.get('DEBUG')).toBe(true);
+  expect(env.get('EMPTY_VALUE')).toBe(undefined);
+  expect(env.get('QUOTED_EMPTY_VALUE')).toBe('');
+  expect(env.get('SINGLE_QUOTED_EMPTY_VALUE')).toBe('');
+  expect(env.get('SPACED_KEY')).toBe("spaced_value");
+  expect(env.get('SPACED_KEY_WITH_QUOTES')).toBe(" spaced_value ");
+  expect(env.get('SPECIAL_CHARS_IN_VALUE')).toBe("!@#$%^&*()_+");
+  expect(env.get('TRAILING_SPACES')).toBe("trailing_spaces");
+  expect(env.get('LIST_OF_VALUES_WITH_QUOTES')).toEqual(['0', '1']);
+  expect(env.get('LIST_OF_VALUES_WITH_SINGLE_QUOTES')).toEqual([' example', "example"]);
+  expect(env.get('LIST_OF_VALUES_WITHOUT_QUOTES')).toEqual(["example", "example"]);
+  expect(env.get('OBJECT')).toEqual({ key: "value" });
+  expect(env.get('COMMENTED_ENV')).toBe(undefined);
+  expect(env.get('SEMI_COMMENTED_ENV')).toBe("sh");
+
+  const allEnvs = env.getAll();
+  console.log(allEnvs);
+});
